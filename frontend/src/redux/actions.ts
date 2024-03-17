5c4392efd19c17ecd30bf61f001d0c07e3052a71
jeffcorpuz <jeffcorpuz@users.noreply.github.com>
2024-03-17 10:44:10 -0400

    initial commit

--- frontend/src/redux/actions.ts
+++
@@ -1,6 +0,0 @@
-export const SET_LOCATION = 'SET_LOCATION';
-
-export const setLocation = (location: string) => ({
-  type: SET_LOCATION,
-  payload: location,
-});
--- frontend/src/redux/store.ts
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import appReducer from './reducer';
-
-const store = configureStore(appReducer);
-
-export default store;
